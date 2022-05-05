0d997d95065b36f3488000ecbe91346da14f4c38
m-ghiringhelli <mghiring@gmail.com>
2022-05-05 12:34:37 -0700

    added auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ProvideAuth } from "./context/ProvideAuth";
+import EntryList from "./views/EntryList/EntryList";
 import Login from "./views/Login/Login";
 
 export default function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
+      <ProvideAuth>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <EntryList />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </ProvideAuth>
     </>
   );
 }
