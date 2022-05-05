a46492dfd5abc6b16fcf247e2dd560beb7694a29
m-ghiringhelli <mghiring@gmail.com>
2022-05-04 17:33:59 -0700

    login allows user display signin or signup

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import AuthForm from '../../components/AuthForm/AuthForm'
+import React, { useState } from 'react';
+import style from './Login.css';
 
 export default function Login() {
+  const [authType, setAuthType] = useState('signin');
+
   return (
     <>
-      <AuthForm />
+      {authType === 'signin' ? (
+        <>
+          <p>Sign into your account</p>
+          <p>Are you new around here? 
+            <span onClick={() => setAuthType('signup')}> Click here to create an account</span>
+          </p>
+        </>
+      ) : ( 
+        <>
+          <p>Create an account</p>
+          <p>Not your first rodeo?
+            <span onClick={() => setAuthType('signin')}> Click here to sign-in to your account</span>
+          </p>
+        </>
+      )}
+      <form className={style.authForm}>
+        <label>
+          <input
+            id='email'
+            name='email'
+            type='email'
+            placeholder='Email address'
+          />
+        </label>
+        <label>
+          <input
+            id='password'
+            name='password'
+            type='password'
+            placeholder='Password'
+          />
+        </label>
+      </form>
     </>
   )
 }
